ba71c63b114d9bb30cecd4195b0a54f98b4e717a
alexisCastilloDev <amcs301004@fpuna.edu.py>
2025-08-17 15:23:17 -0300

    agregando backend

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import app from "./app.js"
+const port = process.env.PORT || 4000 
 
-app.listen("https://backend-greenland.onrender.com")
-
-console.log("Server is running on port 3000")
-
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
